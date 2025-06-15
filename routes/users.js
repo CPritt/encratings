633054f5af85af06fd5c5c26fe4889fdd389b8cb
Chandler <cpritt@charlotte.edu>
2025-06-15 00:13:05 -0400

    first commit

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+
+//getting all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+//getting one user
+router.get('/:id', (req, res) => {
+    res.send('Get one User')
+})
+
+//creating a user
+router.post('/', async (req, res) => {
+    const user = new User({
+        userName: req.body.userName,
+        password: req.body.password,
+        email: req.body.email
+    })
+
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+//updating a user
+router.patch('/:id', (req, res) => {
+
+})
+
+//deleting a user
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+
+
+
+module.exports = router;
